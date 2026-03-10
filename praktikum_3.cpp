75539ef2c922969e70ee771890939137df709a7c
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-10 22:18:00 +0700

    fungsi menghitung BMI

--- praktikum_3.cpp
+++ praktikum_3.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Prosedur input
 void inputData(float &berat, float &tinggi)
 {
     cout << "Masukkan berat badan (kg): ";
@@ -9,4 +8,10 @@ void inputData(float &berat, float &tinggi)
 
     cout << "Masukkan tinggi badan (meter): ";
     cin >> tinggi;
+}
+
+// Fungsi menghitung BMI
+float hitungBMI(float berat, float tinggi)
+{
+    return berat / (tinggi * tinggi);
 }
